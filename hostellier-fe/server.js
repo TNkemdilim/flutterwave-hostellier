d520135002713a7664af962ff4744d0701f5bc55
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-08-02 23:37:47 +0100

    Chore(Stage): Prepare FE for Heroku Deployment

--- hostellier-fe/server.js
+++ hostellier-fe/server.js
@@ -2,8 +2,10 @@
 var express = require("express");
 var path = require("path");
 var serveStatic = require("serve-static");
-var app = express();
+var history = require("connect-history-api-fallback");
+app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, "/dist")));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log("starting project at " + port);
+console.log("server started " + port);
