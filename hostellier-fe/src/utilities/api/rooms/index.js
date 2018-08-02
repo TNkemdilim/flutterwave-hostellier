cf79acb0dd00fbbc6172c1cb58e6537bd6fad0c0
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-08-03 00:45:06 +0100

    Fix(FE/Endpoints): Update Api Query Endpoints In FE

--- hostellier-fe/src/utilities/api/rooms/index.js
+++ hostellier-fe/src/utilities/api/rooms/index.js
@@ -1,45 +1,19 @@
 import HTTP from "@/helpers/http";
 import * as Endpoints from "../../api/endpoints";
+import OffCampusRooms from "./offCampus";
+import OnCampusRooms from "./onCampus";
 
-class RoomsApi {
-  /**
-   * Get all room that exist.
-   * @param {Object} formData Student data
-   */
-  static async getAllRooms(formData) {
-    try {
-      var response = await HTTP.post(Endpoints.allCampusRooms, formData);
-      return response.data;
-    } catch (e) {
-      return e.response.data;
-    }
+/**
+ * Get all room that exist.
+ * @param {Object} formData Student data
+ */
+const getAllRooms = async formData => {
+  try {
+    var response = await HTTP.post(Endpoints.allCampusRooms, formData);
+    return response.data;
+  } catch (e) {
+    return e.response.data;
   }
+};
 
-  /**
-   * Login a student.
-   * @param {Object} formData Student data
-   */
-  static async getAllOffCampusRooms(formData) {
-    try {
-      var response = await HTTP.post(Endpoints.allOffCampusRooms, formData);
-      return response.data;
-    } catch (e) {
-      return e.response.data;
-    }
-  }
-
-  /**
-   * Login a student.
-   * @param {Object} formData Student data
-   */
-  static async getAllOnCampusRooms(formData) {
-    try {
-      var response = await HTTP.post(Endpoints.allOnCampusRooms, formData);
-      return response.data;
-    } catch (e) {
-      return e.response.data;
-    }
-  }
-}
-
-export default RoomsApi;
+export { getAllRooms, OnCampusRooms, OffCampusRooms };
