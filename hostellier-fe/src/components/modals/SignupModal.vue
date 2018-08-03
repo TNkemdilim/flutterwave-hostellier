42f569d14e1c3723cf0cc9960d27941187accb4e
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-08-03 08:55:58 +0100

    Fix(Registration): Query Backend During Registration

--- hostellier-fe/src/components/modals/SignupModal.vue
+++ hostellier-fe/src/components/modals/SignupModal.vue
@@ -5,46 +5,87 @@
         <div class="modal-content">
           <div class="modal-body">
             <div class="col-sm-12 col-md-12 text-center">
-              <h1 class="login-title">Sign in to Hostellier</h1>
+              <h1 class="login-title">Register on Hostellier</h1>
               <p class="login-title-caption">
                 <em>Fatest way to book hostels on and out of campus.</em>
               </p>
               <div class="account-wall">
-                <img class="profile-img" :src="userImage" alt="">
+                <img class="profile-img" :src="'https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120'" alt="">
                 <form class="form-signin">
-                  <input type="text" class="form-control" placeholder="Email" required autofocus>
-                  <input type="password" class="form-control" placeholder="Password" required>
-                  <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    Sign in
-                  </button>
+                  <input type="text" v-model="formData.firstname" class="form-control" placeholder="Firstname" required autofocus>
+                  <input type="text" v-model="formData.lastname" class="form-control" placeholder="Lastname" required>
+                  <select v-model="formData.level" class="form-control form-control-lg" id="inlineFormCustomSelect">
+                    <option value="" selected="selected" disabled >Academic level</option>
+                    <option v-for="(level, index) in [1, 2,3,4]" :key=index :value=level>{{ level * 100}}</option>
+                  </select>
+                  <input v-model="formData.email" type="text" class="form-control" placeholder="Email" required autofocus>
+                  <input v-model="formData.password" type="password" class="form-control" placeholder="Password" required>
+                  <input v-model="formData.c_password" type="password" class="form-control" placeholder="Password Confirmation" required>
+                  
                   <br>
+                  <button @click="registerStudent($event)" class="btn btn-lg btn-primary btn-block">
+                    Register
+                  </button>
                   <label class="checkbox pull-left">
                       <input type="checkbox" value="remember-me">
                       Remember me
                   </label>
                   <br>
-                  <a href="#" class="text-center new-account">Create an account </a>
+                  <a href="#" class="text-center new-account">Login</a>
                 </form>
               </div>
-                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                <a data-toggle="modal" data-target="#loginModal" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <login-modal />
   </div>
 </template>
 
 <script>
 const USER_IMAGE =
   "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120";
+import LoginModal from "@/components/modals/LoginModal.vue";
+import { StudentAuth } from "../../services/backendApi/auth/index.js";
 
 export default {
   name: "signup-modal",
+  components: {
+    LoginModal
+  },
   data() {
     return {
-      userImage: USER_IMAGE
+      userImage: USER_IMAGE,
+      formData: {
+        email: null,
+        password: null,
+        c_password: null,
+        firstname: null,
+        lastname: null,
+        level: null
+      }
     };
+  },
+  methods: {
+    registerStudent: async function(event) {
+      event.preventDefault();
+      this.$loading("Registering student.");
+      let result = await StudentAuth.register(this.formData);
+
+      this.$loading.close();
+      if (result.status === true) {
+        this.$toasted.success(result.message);
+        this.closeModalNow();
+      } else {
+        this.$toasted.error(result.message);
+      }
+    },
+    closeModalNow: function() {
+      this.closeModal = true;
+    }
   }
 };
 </script>
